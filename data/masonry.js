21836960be602d5fcb9549485f0fb03eb448c048
Komár Csongor <csongi8916@gmail.com>
2024-10-23 18:19:18 +0200

    Extend data

--- data/masonry.js
+++ data/masonry.js
@@ -10,49 +10,49 @@ import img009 from '../assets/009.jpg';
 import img010 from '../assets/010.jpg';
 import img011 from '../assets/011.jpg';
 
-const masonryData = [
+const data = [
   {
     maxHeight: 70,
     data: [
-      { id: 1, image: img010, height: 25 },
-      { id: 2, image: img003, height: 10 },
-      { id: 3, image: img002, height: 15 },
-      { id: 4, image: img001, height: 20 },
+      { id: 1, title:'Lorem Ipsum', image: img010, height: 25, date: '2025 July', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur nec eros vel augue varius tempus. Donec varius dignissim accumsan. Cras venenatis lacinia finibus. Curabitur ac gravida massa, in tincidunt massa. Nulla at diam vitae orci mattis mattis. Donec sit amet nisl id risus posuere tempus.' },
+      { id: 2, title:'Lorem Ipsum', image: img003, height: 10, date: '2025 June', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur nec eros vel augue varius tempus. Donec varius dignissim accumsan. Cras venenatis lacinia finibus. Curabitur ac gravida massa, in tincidunt massa. Nulla at diam vitae orci mattis mattis. Donec sit amet nisl id risus posuere tempus.' },
+      { id: 3, title:'Lorem Ipsum', image: img002, height: 15, date: '2025 May', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur nec eros vel augue varius tempus. Donec varius dignissim accumsan. Cras venenatis lacinia finibus. Curabitur ac gravida massa, in tincidunt massa. Nulla at diam vitae orci mattis mattis. Donec sit amet nisl id risus posuere tempus.' },
+      { id: 4, title:'Lorem Ipsum', image: img001, height: 20, date: '2025 April', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur nec eros vel augue varius tempus. Donec varius dignissim accumsan. Cras venenatis lacinia finibus. Curabitur ac gravida massa, in tincidunt massa. Nulla at diam vitae orci mattis mattis. Donec sit amet nisl id risus posuere tempus.' },
     ]
   },
   {
     data: [
-      { id: 5, image: img003, height: 10 },
-      { id: 6, image: img001, height: 18 },
-      { id: 7, image: img004, height: 10 },
-      { id: 8, image: img011, height: 17 },
-      { id: 9, image: img004, height: 15 },
+      { id: 5, title:'Lorem Ipsum', image: img003, height: 10, date: '2025 March', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur nec eros vel augue varius tempus. Donec varius dignissim accumsan. Cras venenatis lacinia finibus. Curabitur ac gravida massa, in tincidunt massa. Nulla at diam vitae orci mattis mattis. Donec sit amet nisl id risus posuere tempus.' },
+      { id: 6, title:'Lorem Ipsum', image: img001, height: 18, date: '2025 February', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur nec eros vel augue varius tempus. Donec varius dignissim accumsan. Cras venenatis lacinia finibus. Curabitur ac gravida massa, in tincidunt massa. Nulla at diam vitae orci mattis mattis. Donec sit amet nisl id risus posuere tempus.' },
+      { id: 7, title:'Lorem Ipsum', image: img004, height: 10, date: '2025 January', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur nec eros vel augue varius tempus. Donec varius dignissim accumsan. Cras venenatis lacinia finibus. Curabitur ac gravida massa, in tincidunt massa. Nulla at diam vitae orci mattis mattis. Donec sit amet nisl id risus posuere tempus.' },
+      { id: 8, title:'Lorem Ipsum', image: img011, height: 17, date: '2024 December', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur nec eros vel augue varius tempus. Donec varius dignissim accumsan. Cras venenatis lacinia finibus. Curabitur ac gravida massa, in tincidunt massa. Nulla at diam vitae orci mattis mattis. Donec sit amet nisl id risus posuere tempus.' },
+      { id: 9, title:'Lorem Ipsum', image: img004, height: 15, date: '2024 November', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur nec eros vel augue varius tempus. Donec varius dignissim accumsan. Cras venenatis lacinia finibus. Curabitur ac gravida massa, in tincidunt massa. Nulla at diam vitae orci mattis mattis. Donec sit amet nisl id risus posuere tempus.' },
     ]
   },
   {
     data: [
-      { id: 10, image: img002, height: 10 },
-      { id: 11, image: img006, height: 10 },
-      { id: 12, image: img011, height: 25 },
-      { id: 13, image: img005, height: 15 },
-      { id: 14, image: img002, height: 10 },
+      { id: 10, title:'Lorem Ipsum', image: img002, height: 10, date: '2024 October', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur nec eros vel augue varius tempus. Donec varius dignissim accumsan. Cras venenatis lacinia finibus. Curabitur ac gravida massa, in tincidunt massa. Nulla at diam vitae orci mattis mattis. Donec sit amet nisl id risus posuere tempus.' },
+      { id: 11, title:'Lorem Ipsum', image: img006, height: 10, date: '2024 September', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur nec eros vel augue varius tempus. Donec varius dignissim accumsan. Cras venenatis lacinia finibus. Curabitur ac gravida massa, in tincidunt massa. Nulla at diam vitae orci mattis mattis. Donec sit amet nisl id risus posuere tempus.' },
+      { id: 12, title:'Lorem Ipsum', image: img011, height: 25, date: '2024 August', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur nec eros vel augue varius tempus. Donec varius dignissim accumsan. Cras venenatis lacinia finibus. Curabitur ac gravida massa, in tincidunt massa. Nulla at diam vitae orci mattis mattis. Donec sit amet nisl id risus posuere tempus.' },
+      { id: 13, title:'Lorem Ipsum', image: img005, height: 15, date: '2024 July', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur nec eros vel augue varius tempus. Donec varius dignissim accumsan. Cras venenatis lacinia finibus. Curabitur ac gravida massa, in tincidunt massa. Nulla at diam vitae orci mattis mattis. Donec sit amet nisl id risus posuere tempus.' },
+      { id: 14, title:'Lorem Ipsum', image: img002, height: 10, date: '2024 June', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur nec eros vel augue varius tempus. Donec varius dignissim accumsan. Cras venenatis lacinia finibus. Curabitur ac gravida massa, in tincidunt massa. Nulla at diam vitae orci mattis mattis. Donec sit amet nisl id risus posuere tempus.' },
     ]
   },
   {
     data: [
-      { id: 15, image: img011, height: 25 },
-      { id: 16, image: img002, height: 15 },
-      { id: 17, image: img006, height: 10 },
-      { id: 18, image: img009, height: 20 },
+      { id: 15, title:'Lorem Ipsum', image: img011, height: 25, date: '2024 May', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur nec eros vel augue varius tempus. Donec varius dignissim accumsan. Cras venenatis lacinia finibus. Curabitur ac gravida massa, in tincidunt massa. Nulla at diam vitae orci mattis mattis. Donec sit amet nisl id risus posuere tempus.' },
+      { id: 16, title:'Lorem Ipsum', image: img002, height: 15, date: '2024 April', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur nec eros vel augue varius tempus. Donec varius dignissim accumsan. Cras venenatis lacinia finibus. Curabitur ac gravida massa, in tincidunt massa. Nulla at diam vitae orci mattis mattis. Donec sit amet nisl id risus posuere tempus.' },
+      { id: 17, title:'Lorem Ipsum', image: img006, height: 10, date: '2024 March', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur nec eros vel augue varius tempus. Donec varius dignissim accumsan. Cras venenatis lacinia finibus. Curabitur ac gravida massa, in tincidunt massa. Nulla at diam vitae orci mattis mattis. Donec sit amet nisl id risus posuere tempus.' },
+      { id: 18, title:'Lorem Ipsum', image: img009, height: 20, date: '2024 February', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur nec eros vel augue varius tempus. Donec varius dignissim accumsan. Cras venenatis lacinia finibus. Curabitur ac gravida massa, in tincidunt massa. Nulla at diam vitae orci mattis mattis. Donec sit amet nisl id risus posuere tempus.' },
     ]
   },
   {
     data: [
-      { id: 19, image: img010, height: 25 },
-      { id: 20, image: img005, height: 15 },
-      { id: 21, image: img008, height: 30 },
+      { id: 19, title:'Lorem Ipsum', image: img010, height: 25, date: '2024 January', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur nec eros vel augue varius tempus. Donec varius dignissim accumsan. Cras venenatis lacinia finibus. Curabitur ac gravida massa, in tincidunt massa. Nulla at diam vitae orci mattis mattis. Donec sit amet nisl id risus posuere tempus.' },
+      { id: 20, title:'Lorem Ipsum', image: img005, height: 15, date: '2023 December', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur nec eros vel augue varius tempus. Donec varius dignissim accumsan. Cras venenatis lacinia finibus. Curabitur ac gravida massa, in tincidunt massa. Nulla at diam vitae orci mattis mattis. Donec sit amet nisl id risus posuere tempus.' },
+      { id: 21, title:'Lorem Ipsum', image: img008, height: 30, date: '2023 November', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur nec eros vel augue varius tempus. Donec varius dignissim accumsan. Cras venenatis lacinia finibus. Curabitur ac gravida massa, in tincidunt massa. Nulla at diam vitae orci mattis mattis. Donec sit amet nisl id risus posuere tempus.' },
     ]
   },
 ];
 
-export default masonryData;
+export default data;
